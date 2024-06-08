a28580095977b18239c2bb5dc95c0e495af55f9e
Sebastian Smith <horizon_mode@outlook.com>
2024-06-08 08:03:54 +0100

    Update playwright.config.ts

--- src/playwright.config.ts
+++ src/playwright.config.ts
@@ -11,6 +11,8 @@ import { defineConfig, devices } from '@playwright/test'
  */
 export default defineConfig({
   testDir: './__e2e-tests__',
+  /* Ignore Jest */
+  testIgnore: './__unit-tests__',
   /* Run tests in files in parallel */
   fullyParallel: true,
   /* Fail the build on CI if you accidentally left test.only in the source code. */
