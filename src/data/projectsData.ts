c92e6633cbb7eb4c67b6cdad2c84c495e3b49ba8
Sebastian Smith <horizon_mode@outlook.com>
2024-11-28 10:57:23 +0000

    add IRE

--- src/data/projectsData.ts
+++ src/data/projectsData.ts
@@ -12,6 +12,12 @@ const projectsData: Project[] = [
     imgSrc: '/static/images/gym.jpeg',
     href: '/blog/blitz-gym',
   },
+  {
+    title: 'International Rubgy Experience x OMM',
+    description: `An immersive museum experience on the history of Rugby.`,
+    imgSrc: '/static/images/ire.jpg',
+    href: '/blog/international-rugby-experience',
+  },
 ]
 
 export default projectsData
